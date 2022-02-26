facf4d1923d6ec129a3a461b57f1c0f1d8be5118
Marcus Koppelmann <mkoppelmann98@gmail.com>
2022-02-27 00:06:57 +0100

    (GH-3) refactored LanguageTagger

--- Codebase/equaly/src/main/java/org/hwr/equaly/controller/languageTagger/LanguageTagger.java
+++ Codebase/equaly/src/main/java/org/hwr/equaly/controller/languageTagger/LanguageTagger.java
@@ -3,5 +3,6 @@ package org.hwr.equaly.controller.languageTagger;
 import com.github.pemistahl.lingua.api.Language;
 
 public interface LanguageTagger {
+    void initialize();
     Language getLanguage(String text);
 }
--- Codebase/equaly/src/main/java/org/hwr/equaly/controller/languageTagger/LanguageTaggerImpl.java
+++ Codebase/equaly/src/main/java/org/hwr/equaly/controller/languageTagger/LanguageTaggerImpl.java
@@ -1,6 +1,7 @@
 package org.hwr.equaly.controller.languageTagger;
 
 import com.github.pemistahl.lingua.api.Language;
+import com.github.pemistahl.lingua.api.LanguageDetector;
 import com.github.pemistahl.lingua.api.LanguageDetectorBuilder;
 
 import static com.github.pemistahl.lingua.api.Language.*;
@@ -8,9 +9,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LanguageTaggerImpl implements LanguageTagger {
-    private com.github.pemistahl.lingua.api.LanguageDetector detector;
+    private LanguageDetector detector;
 
-    public LanguageTaggerImpl() {
+    public void initialize() {
         this.detector = LanguageDetectorBuilder.fromLanguages(ENGLISH, GERMAN).build();
     }
 
