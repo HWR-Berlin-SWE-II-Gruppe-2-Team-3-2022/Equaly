47c10cbed9a69b2be0f53a2e3e4837438f8b1fbd
Marcus Koppelmann <mkoppelmann98@gmail.com>
2022-02-27 12:08:30 +0100

    (GH-3) added Fragment class

---
+++ Codebase/equaly/src/main/java/org/hwr/equaly/model/Fragment.java
@@ -0,0 +1,13 @@
+package org.hwr.equaly.model;
+
+public class Fragment {
+    public String token;
+    public String tag;
+    public long index;
+
+    public Fragment(String token, String tag, long index) {
+        this.token = token;
+        this.tag = tag;
+        this.index = index;
+    }
+}
